d2be612eeb55e1760dcbff4e73553ed650c14637
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2020-06-17 18:51:52 +0200

    WIP: show recv_timeout API
    TODO update text

--- beginner/apps/src/bin/radio-recv.rs
+++ beginner/apps/src/bin/radio-recv.rs
@@ -4,9 +4,8 @@
 
 use core::str;
 
-use cortex_m::asm;
 use cortex_m_rt::entry;
-use dk::ieee802154::Packet;
+use dk::ieee802154::{Error, Packet};
 use panic_log as _; // the panicking behavior
 
 #[entry]
@@ -14,17 +13,27 @@ fn main() -> ! {
     // initializes the peripherals
     let board = dk::init().unwrap();
     let mut radio = board.radio;
+    let mut timer = board.timer;
 
     let mut packet = Packet::new();
     let msg = b"olleh";
     packet.copy_from_slice(msg);
     radio.send(&packet);
     log::info!("sent: {:?}", msg);
-    let crc = radio.recv(&mut packet);
-    let s = str::from_utf8(&*packet).expect("response is not valid UTF-8");
-    log::info!("received: {} (CRC={:?})", s, crc);
+    let timeout = 1_000;
+    let res = radio.recv_timeout(&mut packet, &mut timer, timeout);
 
-    loop {
-        asm::bkpt();
+    match res {
+        Ok(crc) => {
+            log::info!(
+                "received: {} (CRC={})",
+                str::from_utf8(&*packet).expect("response is not valid UTF-8"),
+                crc
+            );
+        }
+        Err(Error::Crc(crc)) => log::error!("invalid CRC: {:06x}", crc),
+        Err(Error::Timeout) => log::error!("no response within {} ms", timeout),
     }
+
+    dk::exit()
 }
