e8ed24d893115b4c2f0000b629b0ebfaaf17c6ce
Lotte Steenbrink <lotte.steenbrink@ferrous-systems.com>
2021-04-12 17:09:38 +0200

    start converting advanced apps

--- advanced/firmware/src/bin/hello.rs
+++ advanced/firmware/src/bin/hello.rs
@@ -3,14 +3,15 @@
 
 use cortex_m::asm;
 use cortex_m_rt::entry;
-use panic_log as _; // panic handler
+// this imports `beginner/apps/lib.rs` to retrieve our global logger + panicking-behavior
+use firmware as _;
 
 #[entry]
 fn main() -> ! {
     // board initialization
     dk::init().unwrap();
 
-    log::info!("Hello, world!");
+    defmt::info!("Hello, world!");
 
     loop {
         asm::bkpt();
