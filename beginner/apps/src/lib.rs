36776c7ba8582327e0262fdc1bba1fd98188c6fa
Lotte Steenbrink <lotte.steenbrink@ferrous-systems.com>
2021-06-04 14:20:24 +0200

    rm superfluous exit()

--- beginner/apps/src/lib.rs
+++ beginner/apps/src/lib.rs
@@ -8,10 +8,3 @@ use panic_probe as _;
 fn panic() -> ! {
     cortex_m::asm::udf()
 }
-
-/// Terminates the application and makes `probe-run` exit with exit-code = 0
-pub fn exit() -> ! {
-    loop {
-        cortex_m::asm::bkpt();
-    }
-}
