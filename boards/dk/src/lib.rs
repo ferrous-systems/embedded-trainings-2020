59f20c1c05c6c9591847b0e95afdf6c394dde7f1
Mirabellensaft <tanks@ferrous-systems.com>
2022-01-12 17:02:33 +0100

    fix dangling usb

--- boards/dk/src/lib.rs
+++ boards/dk/src/lib.rs
@@ -290,6 +290,16 @@ fn RTC0() {
 /// Exits the application and prints a backtrace when the program is executed through the `probe-run`
 /// Cargo runner
 pub fn exit() -> ! {
+    unsafe {
+        // turn off the USB D+ pull-up before pausing the device with a breakpoint
+        // this disconnects the nRF device from the USB host so the USB host won't attempt further
+        // USB communication (and see an unresponsive device). probe-run will also reset the nRF's
+        // USBD peripheral when it sees the device in a halted state which has the same effect as
+        // this line but that can take a while and the USB host may issue a power cycle of the USB
+        // port / hub / root in the meantime, which can bring down the probe and break probe-run
+        const USBD_USBPULLUP: *mut u32 = 0x4002_7504 as *mut u32;
+        USBD_USBPULLUP.write_volatile(0)
+    }
     defmt::println!("`dk::exit()` called; exiting ...");
     // force any pending memory operation to complete before the BKPT instruction that follows
     atomic::compiler_fence(Ordering::SeqCst);
